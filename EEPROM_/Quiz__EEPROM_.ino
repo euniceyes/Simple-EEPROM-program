164151142dad9893c813463537db6272cd23da64
euniceyes <64119283+euniceyes@users.noreply.github.com>
2020-08-27 16:42:02 +0800

    Add files via upload
    This program requires an Arduino connected to a circuit board or a circuit made on Proteus in order to see the output through hardware or software respectively. Please refer to the jpg file attached for your circuit. Place the button and LED to its assigned pin number.

---
+++ EEPROM_/Quiz__EEPROM_.ino
@@ -0,0 +1,53 @@
+#include <EEPROM.h>
+
+bool flag = false;
+int x =0;
+
+int eunice;
+
+void setup() {
+  pinMode (A0, INPUT); //button
+  pinMode (2, OUTPUT); //LED
+
+  digitalWrite(A0, LOW);
+
+  eunice = EEPROM.read(0);
+
+  if( eunice == 0){
+    digitalWrite(2, LOW);
+  }
+  else if(eunice == 1){
+    digitalWrite(2, HIGH);
+  }
+}
+
+void loop() {
+   if(x == 0){
+     if( digitalRead(A0) == HIGH){
+        flag = true;
+     }  
+     if( digitalRead(A0) == LOW and flag == true){
+        digitalWrite(2, HIGH);
+        x++;
+        flag = false;
+
+        EEPROM.write(0, 1);
+      }
+   }// if x ==0
+
+   
+   if (x == 1){
+     if( digitalRead(A0) == HIGH){
+        flag = true;
+     }  
+  
+     if( digitalRead(A0) == LOW and flag == true){
+        digitalWrite(2, LOW);
+        x = 0;
+        flag = false;
+
+        EEPROM.write(0, 0);
+      }
+   }// if x ==1
+  
+}//end void loop
